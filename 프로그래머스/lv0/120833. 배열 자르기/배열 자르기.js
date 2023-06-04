43240fb95c17e4ea11198962a772a355a389718c
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-04 23:27:44 +0900

    [level 0] Title: 배열 자르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,6 +1,8 @@
 function solution(numbers, num1, num2) {
-    var answer = numbers.filter(function(element, index, array) {
-        return (num1 <= index) && (index <= num2);
-    });
+    // var answer = numbers.filter(function(element, index) {
+    //     return (num1 <= index) && (index <= num2);
+    // });
+    // return answer;
+    var answer = numbers.slice(num1, num2 + 1);
     return answer;
 }
