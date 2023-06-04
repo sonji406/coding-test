e671211fcec06777b0cf83c99f1edf8982248d1c
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-05 01:44:55 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,13 +1,24 @@
+// function solution(n) {
+//     var answer = 0;
+//     let firstNum = 1;
+    
+//     while (firstNum <= n) {
+//         if (n % firstNum === 0) {
+//             answer++;
+//         }
+//         firstNum++;
+//     }
+//     return answer;
+// }
 function solution(n) {
     var answer = 0;
     let firstNum = 1;
     
-    while (firstNum <= n) {
+    while (firstNum <= Math.sqrt(n)) {
         if (n % firstNum === 0) {
-            answer++;
+            answer += firstNum === n / firstNum ? 1 : 2;
         }
         firstNum++;
     }
     return answer;
 }
-
