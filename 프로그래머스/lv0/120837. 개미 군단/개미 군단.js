45559262f1ced907982963fa492f3f755b7aadd8
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-06 19:49:42 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,14 @@
+function solution(hp) {
+    var answer = 0;
+    while (hp !== 0) {
+        if (hp >= 5) {
+           hp -= 5;
+        } else if (hp >= 3) {
+            hp -= 3;
+        } else {
+            hp -=1;
+        }
+        answer++;
+    }
+    return answer;
+}
