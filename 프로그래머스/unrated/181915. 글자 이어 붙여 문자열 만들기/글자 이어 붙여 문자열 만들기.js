50b83c45b08e8f387ae4f52e12aee3074b1cfae9
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-08 15:16:05 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,12 +1,18 @@
 function solution(my_string, index_list) {
     var answer = '';
     const stringArray = Array.from(my_string);
-    for (let i = 0; i < index_list.length; i++) {
-        const arrayIndex = index_list[i];
-        if (stringArray[arrayIndex] !== undefined) {
-            answer += stringArray[arrayIndex];
-        }
+    
+    for (const num of index_list) {
+        answer += stringArray[num];  
     }
+    
+    
+//     for (let i = 0; i < index_list.length; i++) {
+//         const arrayIndex = index_list[i];
+//         if (stringArray[arrayIndex] !== undefined) {
+//             answer += stringArray[arrayIndex];
+//         }
+//     }
     
     return answer;
 }
