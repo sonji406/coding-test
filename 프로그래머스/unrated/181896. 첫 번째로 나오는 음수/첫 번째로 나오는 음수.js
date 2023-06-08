bd5ffdc202a6fbc43e34cbad48d6ec77ca3df7ee
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-08 16:05:31 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,10 +1,12 @@
 function solution(num_list) {
-    var answer = 0;
-    for (const [ index, element ] of num_list.entries()) {
-        if (Math.sign(element) === -1) {
-            answer = index;
-            break;
-        } else answer = -1;
-    }
+    var answer = num_list.findIndex((num) => num < 0);
+    
+    // for (const [ index, element ] of num_list.entries()) {
+    //     if (Math.sign(element) === -1) {
+    //         answer = index;
+    //         break;
+    //     } else answer = -1;
+    // }
+    
     return answer;
 }
