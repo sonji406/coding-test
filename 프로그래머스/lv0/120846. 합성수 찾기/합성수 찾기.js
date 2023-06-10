f9ce6b8000b6e66cf49b39e8baac26d974eb27b6
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-11 01:34:42 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        let cnt = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i%j === 0) {
+                cnt++;
+                if (cnt === 3) {
+                    answer++;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
