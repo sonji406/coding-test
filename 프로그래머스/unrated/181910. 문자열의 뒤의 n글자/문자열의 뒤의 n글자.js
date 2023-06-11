1261fa0e9bc51aadd49e631f6955ec5832a2f39d
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-11 19:21:54 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -0,0 +1,9 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for (let i = my_string.length - n; i < my_string.length; i++) {
+        answer += my_string[i];
+    }
+    
+    return answer;
+}
