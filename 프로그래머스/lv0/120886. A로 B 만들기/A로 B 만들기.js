652813b4c4fc5973be414ffb2476e761584c9659
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-11 14:28:29 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,14 +1,3 @@
 function solution(before, after) {
-    var answer = 1;
-    const beforeArr = Array.from(before).sort();
-    const afterArr = Array.from(after).sort();
-    
-    for (let i = 0; i < beforeArr.length; i++) {
-        if (beforeArr[i] !== afterArr[i]) {
-            answer = 0;
-            break;
-        }
-    }
-
-    return answer;
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
