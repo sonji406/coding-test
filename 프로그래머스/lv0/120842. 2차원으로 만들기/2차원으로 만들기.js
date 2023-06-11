c55aea6ee4408a8686fdd9175047447619596d4f
sonji406 <133579214+sonji406@users.noreply.github.com>
2023-06-11 13:14:21 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list, n) {
+    var answer = [];
+    let arr = [];
+    
+    for (let i = 0; i < num_list.length; i++) {
+        arr.push(num_list[i]);
+        
+        if (arr.length === n) {
+            answer.push([...arr]);
+            arr.length = 0;
+        }
+    }
+        
+    return answer;
+}
