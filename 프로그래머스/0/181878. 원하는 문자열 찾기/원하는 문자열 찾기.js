15b0571aaaaafb2cc06f98c1ec68cafcfa95f10d
sonji <133579214+sonji406@users.noreply.github.com>
2024-02-09 22:57:58 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.12 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,3 +1,4 @@
 function solution(myString, pat) { 
-    return myString.toLowerCase().indexOf(pat.toLowerCase()) === -1 ? 0 : 1;
+    //return myString.toLowerCase().indexOf(pat.toLowerCase()) === -1 ? 0 : 1;
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
