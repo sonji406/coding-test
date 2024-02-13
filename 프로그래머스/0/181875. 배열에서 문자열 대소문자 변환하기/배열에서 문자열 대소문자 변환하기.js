313c56ac073cd2e20d24f5dcbdee354cd12d5012
sonji <133579214+sonji406@users.noreply.github.com>
2024-02-13 22:34:13 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
@@ -1,9 +1,13 @@
 function solution(strArr) {
-    const answer = [];
+//     const answer = [];
     
-    strArr.forEach((value, key) => {
-        answer[key] = key%2 === 0 ? value.toLowerCase() : value.toUpperCase();
+//     strArr.forEach((value, key) => {
+//         answer[key] = key%2 === 0 ? value.toLowerCase() : value.toUpperCase();
+//     })
+    
+//     return answer;
+    
+    return strArr.map((v, i) => {
+        return strArr[i] = i%2 === 0 ? v.toLowerCase() : v.toUpperCase();
     })
-    
-    return answer;
 }
