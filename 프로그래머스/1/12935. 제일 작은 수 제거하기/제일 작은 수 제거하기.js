a5dc864845d9a64ee19076027f234225c37266a8
sonji <133579214+sonji406@users.noreply.github.com>
2024-04-03 15:30:11 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,13 +1,19 @@
 function solution(arr) {
-    var answer = [...arr];
-    
-    answer.sort((a, b) => a - b )
-    
-    for (i=0; i<arr.length; i++) {
-        if(arr[i] === answer[0]) {
-            arr.splice(arr.indexOf(arr[i]), 1);
-        }
-    }
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
     
     return arr.length === 0 ? [-1] : arr;
 }
+
+// function solution(arr) {
+//     var answer = [...arr];
+    
+//     answer.sort((a, b) => a - b )
+    
+//     for (i=0; i<arr.length; i++) {
+//         if(arr[i] === answer[0]) {
+//             arr.splice(arr.indexOf(arr[i]), 1);
+//         }
+//     }
+    
+//     return arr.length === 0 ? [-1] : arr;
+// }
