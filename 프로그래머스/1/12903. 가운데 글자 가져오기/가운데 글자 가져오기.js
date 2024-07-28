f5c454bf7ab74bf556be699365d9cc5218c08760
sonji <133579214+sonji406@users.noreply.github.com>
2024-07-28 21:25:17 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer = '';
+    
+    if (s.length % 2 === 0) {
+        answer = s[s.length/2-1]+s[s.length/2]; 
+    } else {
+        answer = s[Math.ceil(s.length/2)-1]
+    }
+    
+    return answer;
+}
